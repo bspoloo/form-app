9d251998b6083a8cc97e849a7a91d747c6ac70db
bspoloo <animetx098@gmail.com>
2025-02-17 00:33:17 -0400

    finish validators forms

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as CustomValidators from '../../../shared/validators/validators.function';
+import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-register-page',
   standalone: false,
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.css'
+  styleUrl: './register-page.component.css',
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private validatorSerice : ValidatorsService, private emailValidatorService : EmailValidatorService) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(validatorSerice.firstNameAndLastnamePattern)]],
+      email: ['', [Validators.required, Validators.pattern(validatorSerice.emailPattern)], [this.emailValidatorService]],
+      username: ['', [Validators.required, validatorSerice.cantBeStrider] ],
+      password1: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required, Validators.minLength(6)]],
+    } , {
+      validators: [
+        this.validatorSerice.isFieldOneEqualFielTwo('password1', 'password2'),
+      ]
+    });
+  }
+  public onSubmit() :void{
+    this.myForm.markAllAsTouched();
+  }
+  public isValidField(fiel : string) : boolean | null{
+    return this.validatorSerice.isValidField(fiel, this.myForm);
+  }
 }
--- src/app/reactive/pages/switch-page/switch-page.component.ts
+++ src/app/reactive/pages/switch-page/switch-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switch-page',
@@ -6,6 +7,53 @@ import { Component } from '@angular/core';
   templateUrl: './switch-page.component.html',
   styleUrl: './switch-page.component.css'
 })
-export class SwitchPageComponent {
+export class SwitchPageComponent implements OnInit{
+  public myForm : FormGroup;
+  public person = {
+    gender : 'M',
+    wantNotifications: true,
+  }
+  constructor(private fb : FormBuilder){
+
+    this.myForm = this.fb.group({
+      gender: ['M', Validators.required],
+      wantNotifications: [true, Validators.required],
+      termsAndCondition: [false, Validators.requiredTrue]
+    });
+
+  }
+  ngOnInit(): void {
+    this.myForm.reset(this.person)
+  }
+
+  public onSave() : void{
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    const {termsAndCondition , ...newPerson} = this.myForm.value;
+    this.person = newPerson;
+
+  }
+  public isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+  public getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) {
+      return null;
+    }
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Debe aceptar las condiciones de termino y uso';
+        default:
+          return null;
+      }
+    }
+    return null;
+  }
 
 }
